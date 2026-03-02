ec4ae9cccaaa1bfc76e9343f490b55651a5777ef
AndersonKho <andersonkho27@gmail.com>
2026-03-02 23:08:00 +0000

    test: add pytest automated tests

--- test_chatbot.py
+++ test_chatbot.py
@@ -4,8 +4,6 @@ import pytest
 
 # Import the Flask app and helper functions
 from app import app, check_for_crisis
-
-
 from app import sanitise_input
 
 
@@ -38,3 +36,26 @@ class TestChatAPI:
         data = response.get_json()
         assert "response" in data
         assert len(data["response"]) > 0
+
+
+class TestInputSanitisation:
+    """Tests for input sanitisation."""
+
+    def test_strips_whitespace(self):
+        """Leading and trailing whitespace should be removed."""
+        assert sanitise_input("  hello  ") == "hello"
+
+    def test_rejects_whitespace_only(self):
+        """Messages with only whitespace should be rejected."""
+        assert sanitise_input("     ") is None
+        assert sanitise_input("\n\t\n") is None
+
+    def test_strips_html_tags(self):
+        """HTML tags should be removed."""
+        assert sanitise_input("<b>hello</b>") == "hello"
+        assert sanitise_input("<script>alert('x')</script>") == "alert('x')"
+
+    def test_rejects_too_long(self):
+        """Messages over 500 chars should be rejected."""
+        assert sanitise_input("a" * 501) is None
+        assert sanitise_input("a" * 500) == "a" * 500
